8162042451e435feba3f629350b80896f632f3d2
qiaojinxia <1158829384@qq.com>
2021-06-03 08:33:20 +0800

    change to sort merkle tree

--- core/merkle.go
+++ core/merkle.go
@@ -1,5 +1,9 @@
 package core
 
+/**
+ * Created by @CaomaoBoy on 2021/6/1.
+ *  email:<115882934@qq.com>
+ */
 import (
 	"crypto/sha256"
 	"math/big"
@@ -9,24 +13,63 @@ import (
 type MerkleTree struct {
 	Root *MerkleNode
 	merkleRoot []byte
+	content []*MerkleNode
 }
 
 type MerkleNode struct {
 	left *MerkleNode
 	right *MerkleNode
 	prev *MerkleNode
+	isleft bool
 	hashData []byte
 }
 
+func(mt *MerkleTree) Verify(txID []byte) [][]byte{
+	index := binarySearch(mt.content,txID)
+	if index == -1{
+		return nil
+	}
+	merklePath := make([][]byte,0,index)
+	var cur = mt.content[index].prev
+	for cur != nil{
+		merklePath = append(merklePath, cur.left.hashData)
+		merklePath = append(merklePath, cur.right.hashData)
+		cur = cur.prev
+	}
+	return merklePath
+}
+
+func binarySearch(content []*MerkleNode,txID []byte) int{
+	left := 0
+	right := len(content)
+	b1 := big.Int{}
+	b1.SetBytes(txID)
+	for left <= right{
+		mid := left + (right - left) / 2
+		b2:= big.Int{}
+		b2.SetBytes(content[mid].hashData)
+		bigOrSmall := b1.Cmp(&b2)
+		if bigOrSmall == 1{	//b1 > b2
+			left = mid + 1
+		}else if bigOrSmall == 0{ //b1 = b2
+			return mid
+		}else{//b1< b2
+			right = mid -1
+		}
+	}
+	return -1
+}
+
 func NewMerkleTree(hashData [][]byte) *MerkleTree{
-	var nodes []MerkleNode
+	var nodes []*MerkleNode
 	if len(hashData) % 2 != 0{
 		hashData = append(hashData, hashData[len(hashData)-1])
 	}
 	for _,v := range hashData{
 		node := NewMerkleNode(nil,nil,v)
-		nodes = append(nodes,*node)
+		nodes = append(nodes,node)
 	}
+	content := nodes
 	sort.SliceStable(nodes, func(i, j int) bool {
 		b1 := big.Int{}
 		b2 := big.Int{}
@@ -35,26 +78,19 @@ func NewMerkleTree(hashData [][]byte) *MerkleTree{
 		return b1.Cmp(&b2) == -1
 	})
 	for ;len(nodes)>1;{
-		var tmpNode []MerkleNode
+		var tmpNode []*MerkleNode
 		for i:=0;i<len(nodes);i+=2{
-			node := NewMerkleNode(&nodes[i],&nodes[i+1],nil)
+			node := NewMerkleNode(nodes[i],nodes[i+1],nil)
 			nodes[i].prev = node
 			nodes[i+1].prev = node
-			tmpNode = append(tmpNode, *node)
+			tmpNode = append(tmpNode, node)
 		}
 		if len(tmpNode) != 1 && len(tmpNode) % 2 != 0{
 			tmpNode = append(tmpNode, tmpNode[len(tmpNode)-1])
 		}
-		sort.SliceStable(tmpNode, func(i, j int) bool {
-			b1 := big.Int{}
-			b2 := big.Int{}
-			b1.SetBytes(nodes[i].hashData)
-			b2.SetBytes(nodes[j].hashData)
-			return b1.Cmp(&b2) == -1
-		})
 		nodes = tmpNode
 	}
-	return &MerkleTree{Root: &nodes[0],merkleRoot: nodes[0].hashData}
+	return &MerkleTree{Root: nodes[0],content:content,merkleRoot: nodes[0].hashData}
 }
 
 func NewMerkleNode(left,right *MerkleNode,data []byte) *MerkleNode{
@@ -63,6 +99,7 @@ func NewMerkleNode(left,right *MerkleNode,data []byte) *MerkleNode{
 		mnode = &MerkleNode{
 			left:     nil,
 			right:    nil,
+			isleft :true,
 			hashData: data,
 		}
 	}else{
--- core/merkle_test.go
+++ core/merkle_test.go
@@ -7,17 +7,22 @@ import (
 )
 
 func Test_MerkleTree(t *testing.T) {
-	c1 := sha256.Sum256([]byte("1231231SADASD2321"))
-	c2 := sha256.Sum256([]byte("123123SADASD12321"))
-	c3 := sha256.Sum256([]byte("12312ASDSADSAD312321"))
+	c1 := sha256.Sum256([]byte("1231231SADAdsfSD2321"))
+	c2 := sha256.Sum256([]byte("123123SADA123SD12321"))
+	c3 := sha256.Sum256([]byte("12312ASDSAD213SAD312321"))
+	c4 := sha256.Sum256([]byte("12312AasdasdSDSADSAD312321"))
 	tree := NewMerkleTree([][]byte{
 		c1[:],
 		c2[:],
 		c3[:],
-		c3[:],
-		c3[:],
+		c4[:],
 	})
-	fmt.Printf("%x\n",tree.Root.left.left.hashData)
-	fmt.Printf("%x\n",tree.Root.left.right.hashData)
-	fmt.Printf("%x\n",tree.Root.hashData)
+	for _,v := range tree.Verify(c3[:]){
+		fmt.Printf("%x\n",v)
+	}
+	fmt.Printf("Merkle Root %x\n",tree.merkleRoot)
+
+	//fmt.Printf("%x\n",tree.Root.left.left.hashData)
+	//fmt.Printf("%x\n",tree.Root.left.right.hashData)
+	//fmt.Printf("%x\n",tree.Root.hashData)
 }
